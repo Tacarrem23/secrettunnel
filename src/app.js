2ff91058ba4dfde632f2abaf96f79154dca96370
Tacarrem23 <carremitchell729@gmail.com>
2025-05-19 22:05:28 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
+// App.js
+import React, { useState } from 'react';
+import { AppContext } from './AppContext';
+import Entrance from './Entrance';
+import Tablet from './Tablet';
+import Tunnel from './Tunnel';
+
+function App() {
+  const [token, setToken] = useState(null);
+  const [location, setLocation] = useState('ENTRANCE');
+
+  return (
+    <AppContext.Provider value={{ token, setToken, location, setLocation }}>
+      {location === 'ENTRANCE' && <Entrance />}
+      {location === 'TABLET' && <Tablet />}
+      {location === 'TUNNEL' && <Tunnel />}
+    </AppContext.Provider>
+  );
+}
+
+export default App;
 
